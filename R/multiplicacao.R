20a401529b2e01d4336251ada135daa9d2f71ec9
Marioslfilho <159330992+Marioslfilho@users.noreply.github.com>
2025-04-18 23:27:06 -0300

    Add files via upload

---
+++ R/multiplicacao.R
@@ -0,0 +1,15 @@
+#' Multiplica dois números
+#'
+#' Esta função retorna a soma de dois números.
+#'
+#' @param a Um número.
+#' @param b Outro número.
+#'
+#' @return Produto de a e b
+#' @examples
+#' soma(2, 3)
+#' @export
+multiplicacao <- function(a, b) {
+  a*b
+}
+help("multiplicacao")
---
+++ R/soma.R
@@ -0,0 +1,14 @@
+#' Soma dois números
+#'
+#' Esta função retorna a soma de dois números.
+#'
+#' @param a Um número.
+#' @param b Outro número.
+#'
+#' @return A soma de a e b.
+#' @examples
+#' soma(2, 3)
+#' @export
+soma <- function(a, b) {
+  a + b
+}
